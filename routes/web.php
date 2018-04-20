3cb0183fa669bd3b73f1ec877e3094dc521defa9
evgesha05 <evgesha_eva_kra97@mail.ru>
2018-04-20 14:18:37 +0400

    Admin: category index page

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,11 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth'] ], function () {
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+    Route::resource('/category', 'CategoryController', ['as'=>'admin']);
 });
+
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
